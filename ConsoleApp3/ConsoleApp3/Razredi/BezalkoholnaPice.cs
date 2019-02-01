412cf5a2ebb3c937d544f11fb678a37ffddebd21
Lucija Safaric <lucija.safaric2311@gmail.com>
2019-02-02 00:22:00 +0100

    Cjenik

---
+++ ConsoleApp3/ConsoleApp3/Razredi/BezalkoholnaPice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Razredi
+{
+    class BezalkoholnaPice:Pice
+    {
+        public BezalkoholnaPice(string opis,int kolicina,double cijena )
+        {
+            this.opis = opis;
+            this.kolicina = kolicina;
+            this.cijena = cijena;
+        }
+
+        public override string Opis()
+        {
+            return base.Opis();
+        }
+    }
+}
